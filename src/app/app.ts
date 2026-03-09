b19404b6d2eaae44c89f40b4c868ca2995696790
Peter Macko <peter.macko@accenture.com>
2026-03-09 20:00:05 +0100

    create scorekeeper using signal forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,72 @@
 import { Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
 import { RouterOutlet } from '@angular/router';
+
+type Score = Array<number | null>;
+
+interface PlayerFormModel {
+  name: string;
+  score: Score;
+}
+
+interface ScorekeeperFormModel {
+  players: PlayerFormModel[];
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormField],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('scorekeeper');
+  scorekeeperModel = signal<ScorekeeperFormModel>({
+    players: [{ name: 'Player 1', score: [null] }],
+  });
+
+  scorekeeperForm = form(this.scorekeeperModel);
+
+  calculateSum(score: Score): number {
+    const sum = score.reduce((acc, curr) => {
+      return (acc ?? 0) + (curr ?? 0);
+    }, 0);
+    return sum ?? 0;
+  }
+
+  addPlayer() {
+    const rounds = this.scorekeeperForm.players[0].score.length;
+    const playerCount = this.scorekeeperForm.players.length;
+
+    this.scorekeeperModel.update((data) => {
+      return {
+        players: [
+          ...data.players,
+          {
+            name: `Player ${playerCount + 1}`,
+            score: new Array(rounds).fill(null),
+          },
+        ],
+      };
+    });
+  }
+
+  addRound() {
+    this.scorekeeperModel.update((data) => {
+      return {
+        players: data.players.map((player) => {
+          return {
+            ...player,
+            score: [...player.score, null],
+          };
+        }),
+      };
+    });
+  }
+
+  scoreChange(index: number) {
+    const rounds = this.scorekeeperForm.players[0].score.length;
+    if (index + 1 === rounds) {
+      this.addRound();
+    }
+  }
 }
